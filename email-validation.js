c18108437de968b50ee5fa7f30e388f94b041406
Skye M <skyemc608@gmail.com>
2021-07-13 15:38:29 -0500

    lotus

--- email-validation.js
+++ email-validation.js
@@ -9,3 +9,4 @@ function ValidateEmail(inputText) {
         }
       }
     }
+   
---
+++ lotus.js
@@ -0,0 +1,5 @@
+function showFlowers()
+var lotus = prompt("How many lotus flowers do you fancy?");
+for (let i = 0; i < lotus; i++) {
+document.write("<img src ='https://i.ibb.co/Y3GyfvV/26-267467-big-image-lotus-flower-png-vector.png'>");
+console.log(i); }
